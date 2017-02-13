65c3925c35c20e7f59ce8d5d7cde5a296c648120
Alec Day <shirojyuu@gmail.com>
2017-02-12 23:59:04 -0500

    Unity Update -> 5.5.1.f1 Fixed state transition to grounded (had to reduce box collider size)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,13 +2,18 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-    public int speed;
+    public float speed;
+    public float runningSpeed;
+    public float checkDist = 3.0f;
     public int jumpStrength;
 
     private Rigidbody2D rb;
     private Animator anim;
 
+    private float speedMod = 1.0f;
+
     private bool grounded;
+    private bool running;
 
 	// Use this for initialization
 	void Start () 
@@ -21,8 +26,10 @@ public class Player : MonoBehaviour {
 	void Update () 
     {
         //Ground Check  
-        Vector2 checkPos = new Vector2(transform.position.x, transform.position.y - GetComponent<BoxCollider2D>().bounds.size.y / 2);
-        RaycastHit2D groundCheck = Physics2D.Raycast(checkPos, -1.0f * Vector2.up, 5.0f);
+        Vector2 checkPos = new Vector2(transform.position.x, transform.position.y - GetComponent<BoxCollider2D>().bounds.size.y / 2.0f);
+
+        RaycastHit2D groundCheck = Physics2D.Raycast(checkPos, checkDist * Vector2.down, checkDist);
+        Debug.DrawRay(checkPos, Vector2.down * checkDist, Color.green);
 
             if (groundCheck != null && groundCheck.collider.tag.Equals("Walkable"))
             {
@@ -51,7 +58,7 @@ public class Player : MonoBehaviour {
 
         }
         anim.SetFloat("XSpeed", Mathf.Abs(xMove * speed));
-        Vector3 totalMovement = new Vector3(xMove * speed, 0, 0);
+        Vector3 totalMovement = new Vector3(xMove * speed * speedMod, 0, 0);
         rb.AddForce(totalMovement);
 
         //Jumping
@@ -62,6 +69,21 @@ public class Player : MonoBehaviour {
             grounded = false;
             anim.SetBool("Grounded", grounded);
 
+        }
+
+        //Running
+        if(Input.GetButtonDown("RunButton") && xMove != 0)
+        {
+            speedMod = runningSpeed;
+            running = true;
+            anim.SetBool("Running", running);
+        }
+
+        if (Input.GetButtonUp("RunButton"))
+        {
+            speedMod = 1.0f;
+            running = false;
+            anim.SetBool("Running", running);
         }
     }
 }
---
+++ Assets/Scripts/TextboxTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextboxTrigger : MonoBehaviour {
+
+    [SerializeField] private string message;
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            GameObject.FindObjectOfType<TextboxHandler>().SetTextToPrint(message);
+            Destroy(gameObject);
+        }
+    }
+}
